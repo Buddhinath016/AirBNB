4077c3ae6994ba42dce22a4ec5d23827c5b4883a
Buddhinath Mahato <buddhinathmahato016@gmail.com>
2024-03-05 00:00:51 +0530

    Day 4

--- init/index.js
+++ init/index.js
@@ -16,7 +16,7 @@ async function main(){
 }
 
 const initDB = async ()=>{
-    // await Listing.deleteMany({});
+    await Listing.deleteMany({});
     await Listing.insertMany(initData.data);
     console.log("Data updated to database");
 }
